f763849e5c6b39d4068dfb2686c57cddd369790a
CM3319 <cmartin319@boyslatin.net>
2024-10-31 12:46:52 +0000

    worked on python java scripting

---
+++ unit3_python/pythonFoundation.py
@@ -0,0 +1,34 @@
+# data types = are thr fundamental building 
+# blocks of python
+
+# intergers = whole numbers
+# strings = any characters wrapped in qoutes
+#boolean = true and flase 
+
+
+# we output data using the pring function
+print(200 * 8)
+print('welcome to python.')
+
+() #round brackets = function
+{} #curly brackets = objects 
+[] #square brakcets = lists
+
+# variables = container for data
+
+name = 'write some text'
+message = 'good morning'
+# concatention = combining strings
+print(message, name)
+
+story ='I will be going to'
+location='New york'
+time ='8:00'
+print(story, location, time)
+
+valA=10
+valB=20
+
+print(valA + valB)
+
+
