6bb47f5f38c13d95bf64fd21f8e2873b599577f2
CM3319 <cmartin319@boyslatin.net>
2024-11-08 13:45:10 +0000

    finished aync assisgnment

---
+++ unit3_python/async.activity.py
@@ -0,0 +1,24 @@
+
+print(Welcome to async assignment )
+
+rules= use variables for the data youre holding,keep names short and simple,use plural nouns like students
+
+1. string concatention is combining strings
+ex: name 'Chris martin'
+
+
+
+Camel case is after every word its capitalized
+ex: loginName
+
+Snake case is when naming a variable every word after
+ the first word is spaced out with an undersocre.
+ ex:Login_name
+
+ pascal case is every new word is always capitalized
+ ex:LoginName
+
+
+print(3 * 100)
+
+x=True
