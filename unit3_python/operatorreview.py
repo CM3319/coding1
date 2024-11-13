b3df6e904b67e017ba8f5b4b4e4e1c48553dafac
CM3319 <cmartin319@boyslatin.net>
2024-11-13 13:45:36 +0000

    learned arithmetic got some notes down as well

---
+++ unit3_python/operatorreview.py
@@ -0,0 +1,35 @@
+ # assisgnment- doing arithmetic after the assisgnment of a value
+# assisgnment is primarily focusedon the =
+
+jeans = 200.0
+jeans= *=0.25
+
+CheckAmount=''
+
+ CheckoutAfterDiscount = 
+ print(jeans)
+ 
+ # arithmetic operators - math related
+
+sendMoney= 25.00
+
+ depsositAmount= 100.0
+ withdrawAmount=0.0
+ 
+ checkingAccountBalance = 0.0
+ savingsAccountBalance =300.0
+
+investmentAcouunt=0.0
+
+//print(depositAmount + savingsAmountBalance)
+
+# tracker runnner app arithmetic operator examples
+firstruntime= 20.00
+secondRunTime= 10.00
+
+runningDifference = firstruntime- secondRunTime
+
+
+ #logical operators 
+
+ # comparsion operator 
