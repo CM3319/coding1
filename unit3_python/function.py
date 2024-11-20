2ac390c7de307979df65e26efbd80c1fa84c9166
CM3319 <cmartin319@boyslatin.net>
2024-11-20 13:44:44 +0000

    got some notes down and learned functions

--- unit3_python/function.py
+++ unit3_python/function.py
@@ -30,3 +30,38 @@ def pbAndj():
       print('step 2. spread the peanut butter on one side of the bread')
       print( step 3. spread the jelly on the other side of the bread)
       print(step 4. put both of them together)
+
+
+def performMath():
+      print(300 * x)
+      print( 'data has been recieved.')
+      print('about to perform multiplicator')
+      print()      
+      
+      # function call- when we write the functions 
+      # name, the computer will run our
+      # instructions
+      
+      
+      perfromMath(2) # the round brackets is the data 
+      # we want our functions to work on.
+      # performMath(14)
+      # performMath(28)
+      
+      def depositMoney():
+            depositAmount= input('please enter the amount you want')
+            checkingAccount= 2000
+            ('deposit put into account successfully')
+            print(depositAmount + checkingAccount)
+      
+      # depositMoney()
+
+      def hamburger():
+            print('create an order of hamburgers')
+
+
+      def milkshake():
+            print('create an order of milkshakes')
+      
+      def chickenfingers():
+            print('create an order of chicken fingers')
