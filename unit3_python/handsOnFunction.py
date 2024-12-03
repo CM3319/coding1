156acb61cf1b89e5994362aeb72bd152e1f24df7
CM3319 <cmartin319@boyslatin.net>
2024-12-03 13:46:24 +0000

    got notes down, class on functions

---
+++ unit3_python/handsOnFunction.py
@@ -0,0 +1,40 @@
+
+# Discuss the anatomy of a fucntion
+
+# This is a function defintion;
+# it tells the computer the instructions
+# on what we want it to do with data.
+
+# data= just means data types
+
+# () curly brackets - these are passing in 
+# data
+
+# when the brackets are in a function
+# defintion, they are called parameters
+# paramerers= placeholder
+
+def customNameFunction(Chris):
+    print('new nickname is : super' + name)
+
+    #when we want to use a function
+    # we call it by writing its name
+    #when we pass data inside of the brackets
+    # it is now called an arguement
+    # arguement = requires facts/real data
+    
+    # Lesson on Conditional Statemetns
+    #conditional stattements involve the 
+    # 'IF' and 'ELSE' keywords.
+    # IF were looking for a condition we can
+    # run or code, otherwise or ELSE it will 
+    # doing something else by default
+
+    def verifyAge(age):
+        if age > 17:
+            print('Congrats! you can buy GTA')
+        else:
+            print('sorry you can not buy gta')
+
+            verifyAge(16)
+
