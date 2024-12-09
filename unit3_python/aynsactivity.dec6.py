ec60648aca3426853ba488ef3c1784fee73daf72
CM3319 <cmartin319@boyslatin.net>
2024-12-09 13:34:46 +0000

    tried to function reverse, got some of the landmark down

---
+++ unit3_python/aynsactivity.dec6.py
@@ -0,0 +1,16 @@
+def reverse_word(word):
+    return word[::-1]
+
+    # Test function
+    print(reverse_word("cheese"))
+    print(reverse_word("bread"))
+
+    def state_landmark(state):
+        landmarks = {
+
+
+
+
+
+            
+        }
