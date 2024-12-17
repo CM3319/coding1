1aa18c5b1d137191bfb5a3fccf171a3866ad1a88
CM3319 <cmartin319@boyslatin.net>
2024-12-17 13:40:00 +0000

    did half of the aync assignment from coming late friday

---
+++ unit3_python/aynsactivitydec13.py
@@ -0,0 +1,11 @@
+def positiveOrNegativenumber():
+    if number > -1:
+        print('this is a postive number')
+    elif number < 0:
+        print('This is a negative number')
+    elif number == 0:
+        print('this is zero.')
+    else:
+        print('number not recognized')
+
+        positiveOrnegative(-1)
