df238816f08d8a28d63499cdc7abeefbb9b158eb
CM3319 <cmartin319@boyslatin.net>
2024-12-18 13:45:02 +0000

    got notes down on asyncdec13 for test

--- unit3_python/aynsactivitydec13.py
+++ unit3_python/aynsactivitydec13.py
@@ -9,3 +9,25 @@ def positiveOrNegativenumber():
         print('number not recognized')
 
         positiveOrnegative(-1)
+
+
+        def shoppingdiscount(membership, itemname, itemprice):
+            if membership == 'superShopper':
+                print('you will get 10 percent off your item')
+                discountAmount = Itemprice * 0.1
+                FinalAmount = itemPrice -discountAmount
+                print(finalAmount)
+            elif membership== 'megaShopper':
+                discountAmount = Itemprice * 0.15
+                FinalAmount = itemPrice -discountAmount
+                print(finalAmount)
+                print('you will get 15 percent off your item')
+            elif membership=='ultraShopper':
+                discountAmount = Itemprice * 0.2
+                FinalAmount = itemPrice -discountAmount
+                print(finalAmount)
+                print('you will get 20 percent off your item') 
+            else:
+                print('you do not have any membership benefits')
+
+            shoppingDiscount('superShopper', 100, airfryer)    
