07959c11de1fb5ad019da8ebc63cdf20d85c0d42
CM3319 <cmartin319@boyslatin.net>
2024-12-19 13:47:16 +0000

    done the extracedit assisgnment

---
+++ unit3_python/extracreditproject.py
@@ -0,0 +1,16 @@
+def HolidaySchedulePlanner(Date):
+    if date == '12-20-24'
+        print('you will be christmas shopping')
+    elif date == 12-21-24
+        print('you will be helping grandma decorate')
+    elif date == 12-23-24
+        print('you will be going to an christmas party')
+    elif date == 12-25-24
+        print('you will be celebrating christmas with family') 
+    elif date == 12-27-24
+        print('you will be going to an playboi carti concert')
+    else:
+        print('you are not doing nothing this day') 
+
+      HolidaySchedulePlanner('12-20-24'):
+
