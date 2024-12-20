5d53e94e91b8e3d0d028df6bb3c7a3a5a44da948
CM3319 <cmartin319@boyslatin.net>
2024-12-20 13:48:03 +0000

    done what i could on the q2 quiz

---
+++ unit3_python/Q2quiz.py
@@ -0,0 +1,44 @@
+1. "a function is an block of reusable codes that perform an action."
+
+2. "a function parameter is placeholders, A function arguement is an function full of facts"
+
+3. "A conditional Statements are whenb you use if/Else to make decisions"
+
+4. "intergers are whole numbers in an data"
+
+5. "boolean data is true or false."
+
+6. "The best operator would be logical operator"
+
+7. "the best operator would be comparsion"
+
+
+
+
+8.  def Githubpassword():
+        if password == '23ERGTHUOIP'
+           print('password is too long.')
+        elif password = 'GHU'
+           print('the password is to short.')
+        elif password == 'POU89G'
+           print('created succesfully.')
+
+Githubpassword('23ERGTHUOIP'): 
+
+
+
+
+
+9.  def IncomeAmounts():
+        if income == '5000 per year'
+           print('you will be taxed 10%')
+           totaltax = Income + 0.3
+           print(totaltax)
+        elif income == '45000 per year'
+            print('you will be taxed')
+            print(totaltax)
+        elif income == '95000'
+            print('you will be taxed 22%')
+            totaltax = Income + 0.3
+            print(totaltax)
+
