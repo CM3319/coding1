1baf4c9770dc7e6793fb60bc8d783b81d1463935
CM3319 <cmartin319@boyslatin.net>
2025-01-08 13:46:32 +0000

    got some notes n prompt down

---
+++ unit 4/pythonLoops.py
@@ -0,0 +1,30 @@
+# function definiton
+
+# create a function that will determine what level of education
+# a college student is in, based on the number of years
+# theyve been in school.
+
+# function definition 
+
+def titleBySchoolYear(year):
+    if year == 1:
+        print('i am a freshmen')
+    elif year == 2:    
+        print('i am a sophomore')
+    elif year == 3:
+        print('i am a junior')
+    elif year == 4
+        print(' i am a senior')    
+    elif year == 5 or year == 6 :
+        print('youre grade school attaining your masters degree')
+    elif year == 7 or year == 8 :
+        print('youre grade school attainting youre doctorate degree')
+
+    else:
+        print('a error has occured')    
+
+
+titleBySchoolYear(8)
+
+
+groceryList= ['greens,pizza,meats']
