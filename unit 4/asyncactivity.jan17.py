cd5faa8f15f3e71a87c6e314c069d7aa1ca6a7c1
CM3319 <cmartin319@boyslatin.net>
2025-01-21 15:26:32 +0000

    finished async

---
+++ unit 4/asyncactivity.jan17.py
@@ -0,0 +1,16 @@
+campingSupplies = ['tent','sleeping bag','flash light','camping knife']
+
+campingSupplies.reverse()
+print(campingSupplies)
+
+
+campingSupplies.insert('5, snacks')
+print(campingSupplies)
+
+list1 = ['tent','sleeping bag','flash light','camping knife']
+list2 = ['marshmellows','gram crackers','chocolate','chicken hot dogs','water',]
+
+list3 = list1 + list2 
+print(list3)
+campingSupplies.pop(flash light)
+print(campingSupplies)
