b81720fca31a8411da6430630bcc3993767ca110
CM3319 <cmartin319@boyslatin.net>
2025-01-22 13:42:13 +0000

    got loops notes

---
+++ unit 4/whileLoops.py
@@ -0,0 +1,30 @@
+# loops- is a construct that repeat itselfs
+# instructions under specific conditions
+
+
+
+# while loop - so long as a condition is true,
+# keep repeating the loop.
+
+x = 2
+y = 'ian'
+# while x == 2:
+# while y == 'ian':     
+    print('while this text will repeat forever')
+
+expiration= input('what is todays date?:')
+
+while expiration == '1/23/25':
+    print('this product has expired.')
+    expiration= input('what is todays date?:')
+
+def tripSavings():
+    accountBalance = 0
+    tripGoal = 8000
+    while accountBalance < tripGoal:
+        int(depositAmoun)t = input('how much do you want to deposit')
+        newBalance = depositAmount + accountBalance
+        print('your new account balance is + str(newBalance)')
+
+        tripSavings()
+
