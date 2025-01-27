fb30fac2552b5043a043cac02dc3d8b369156146
CM3319 <cmartin319@boyslatin.net>
2025-01-27 01:27:00 +0000

    finished aync worked after a error in the soruce control

---
+++ unit 4/asycactivity.jan24.py
@@ -0,0 +1,32 @@
+def printlistReverse(list):
+    while i > 0:
+        numberList = [1,23,56,3,56,3,20,200]
+numberList.reverse()
+print(numberList)
+
+def two_factor_auth():
+
+correct_password = amazonian
+correct_code = 12345
+
+while True:
+    password = input("enter your password:")
+    if password == 
+    correct_password:
+    while True:
+        try:
+            verification_code = float(input("Enter verifcation code:")
+
+            if verification_code = correct_code:
+
+                print("Authentication successful access granted.")
+
+                return
+                else:
+                    print("incorrect verifcation code.")
+
+                    except ValueError:
+                        print("Invalid  verfication. Please enter a number.")
+
+                        else:
+                            print("Incorrect password. Please try again.")
