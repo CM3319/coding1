fc9e6df4d263f98814f0b7554f6a0de1607caee8
CM3319 <cmartin319@boyslatin.net>
2025-01-30 13:46:30 +0000

    finished the work

---
+++ unit 4/problemSolving.py
@@ -0,0 +1,23 @@
+#  1. What is the problem asking you to
+
+# the problem is asking you to make a rock paper scissors ganme
+
+# 2. What are some keywords and phrases ? 
+
+the computer will randomly select one of the three
+
+# 3. Is there input data that the question provides ?
+
+# The game only allows 3 hands
+
+# 4. What is the desired output ?
+
+# inform if the player one or lost
+
+
+# 5. Pseudocode Steps & Additional Notes 
+
+# we need some logic that will determine what option beat same other option 
+
+# Solution
+
