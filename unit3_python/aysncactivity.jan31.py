f6cc4aa3e97a2e3962fcb5ab9124737faac36a34
CM3319 <cmartin319@boyslatin.net>
2025-02-03 13:34:31 +0000

    half way done

---
+++ unit3_python/aysncactivity.jan31.py
@@ -0,0 +1,43 @@
+Boys Latin has hired you to develop a grade checker program for students. Your program should be able to do the following; allow a student to enter a numerical grade, then return the appropriate letter grade. For example; if a student enters the number 80, the program should give back the letter grade "B". Your program should then repeat by asking the user to enter another numerical grade. Each time a user enters another numerical grade, it should perform some calculation to average out the grades and show the new letter grade, For example: if the user enters the number 70, and the 100, they're average should be an 85, which is a B.
+
+In a complete sentence, what is the problem asking you to do ? to create a system where you enter a number and it gives back a grade letter
+
+Where there any parts of the question that don't make sense, if so write them down in complete sentences.
+ if a student enter 7 and 100 it should be 85
+
+Please write down the keywords and phrases that will help you solve this problem ?
+
+# +85=B        # enter a numerical grade= grade letter
+
+Does the problem provide you with any input data? Does it tell you what type of data your program needs to take in ?
+numerical grade
+Does the problem provide you with desired output data? Does it tell you that your program needs to give somthing back to the user ?
+grade letter
+
+Using Pseudocode (fake code), write down Step by step how you think you can solve this problem. Remember, this does not have to be code.
+
+1 # gather all the info i have
+
+2#  enter that data onto the computer
+
+3 # print a function 
+
+4 # Make the computer print the result
+
+
+
+You are working on a game concept with several other engineers and you need to provide them with a simple proof of concept. For now, you have suggested that your game work on a RPS (rock, paper, scissors) loop. When a user plays, rock, paper, or scissors, the computer will randomly select one of the 3 options as well. Based on the results of a match, Your program should conditionally determine who won, loss, or if it was a draw based on the values. The game should only allow the player to play 3 for 3 turns, and inform the player if the won or lost that match at the end (this RPS game is a best of 2 out of 3 format.)
+
+In a complete sentence, what is the problem asking you to do ?
+When you put rock paper and scisssor the computer should select one of the three
+
+Where there any parts of the question that don't make sense, if so write them down in complete sentences.
+NO
+
+Please write down the keywords and phrases that will help you solve this problem ?
+
+Does the problem provide you with any input data? Does it tell you what type of data your program needs to take in ?
+
+Does the problem provide you with desired output data? Does it tell you that your program needs to give somthing back to the user ?
+
+Using Pseudocode (fake code), write down Step by step how you think you can solve this problem. Remember, this does not have to be code.
