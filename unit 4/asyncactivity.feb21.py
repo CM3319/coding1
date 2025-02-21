ca319e1b4901ab2e34f65f00d873816408202264
CM3319 <cmartin319@boyslatin.net>
2025-02-21 13:43:37 +0000

    finished aync

---
+++ unit 4/asyncactivity.feb21.py
@@ -0,0 +1,9 @@
+# the name of my game is going to be alien apocalypse 
+
+# the genre is gtoing to be horror and shooting and fighting
+
+# the objective is to kill all the aliens in space and go up levels until you find the final boss.
+
+# a game called dead space was a apocalyspe game about zombies and aliens. This inspired me because when i first played it, it was a great gam,e on how sci fi it was and the story itself was amazing including the plot.
+
+# the character will be able to move horizontal and vertical and its in 3rd person. The weapons would go from normal guns on earth to special laser guns to defeat the aliens faster and special equiment to not deal damage.
